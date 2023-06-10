8496eb5dd6c58e489ea028bfde58b2c9e080470a
unknown <skuratowaanna@yandex.ru>
2023-06-10 13:14:30 +0300

    Battery levels

---
+++ 002_HT1621/001_Battery_levels/001_Battery_levels.c
@@ -0,0 +1,210 @@
+ /**-------------------------------------------------------------------
+ *\date  10.06.2023
+ *\brief
+ *
+ ***********************************************************************************************
+ * В данном примере подключим, инициализируем дисплей и выведем три уровня заряда батареи на LCD.
+ ***********************************************************************************************
+ *
+ *     M031EC1AE          HT1624
+ *   ------------         ------------
+ *  |            |       |
+ *  |            |       |
+ *  |            |       |
+ *  |        PA.0| ----> | CS
+ *  |        PA.1| ----> | WR
+ *  |        PA.2| ----> | Data
+ *  |        PA.3| ----> | LED+ для работы данного вывода необходимо доработать плату экрана
+ *  |            |       |
+ *  |        +5V | <---> | Vcc
+ *  |        GND | <---> | GND
+ *
+ * Код взят и адаптирован для M031EC1AE (Nuvoton) отсюда.
+ * https://count-zero.ru/2022/ht1621/
+ * LCD Arduino library https://github.com/valerionew/ht1621-7-seg
+ *
+ *
+ * HT1621 – это LCD драйвер, способный управлять 128 элементным (32х4) индикатором. 
+ * Возможность конфигурирования делает HT1620 пригодным для использования во множестве LCD устройств, включая LCD модули и системы дисплеев.
+ * Микросхема при формировании напряжения смещения для питания LCD потребляет совсем небольшой ток.
+ *
+ * ** Код написан в IDE SEGGER Embedded Studio for ARM **
+ **********************************************************************
+ *\ author PivnevNikolay 
+ *\ сode debugging ScuratovaAnna
+ *\brief
+ */
+#include "NuMicro.h"
+#include "stdbool.h"
+#include <stdio.h>
+
+#define WR   1
+#define CS   0
+#define DATA 2
+
+#define BIAS    0x52
+#define SYSDIS  0X00
+#define SYSEN   0X02
+#define LCDOFF  0X04
+#define LCDON   0X06
+#define XTAL    0x28
+#define RC256   0X30
+#define TONEON  0X12
+#define TONEOFF 0X10
+#define WDTDIS1 0X0A
+
+int _buffer[7] = {
+    0x00,
+    0x00,
+    0x00,
+    0x00,
+    0x00,
+    0x00,
+    0x00,
+};
+//********************************************************************
+void wrDATA(uint8_t data, uint8_t len);
+void wrCMD(uint8_t CMD);
+void config(void);
+void clear(void);
+void wrCLR(uint8_t len);
+void wrclrdata(uint8_t addr, uint8_t sdata);
+void update(void);
+void wrone(uint8_t addr, uint8_t sdata);
+void setBatteryLevel(int level);
+
+//********************************************************************
+void SYS_Init(void) {
+  SYS_UnlockReg();
+  //********* Enable HIRC clock (Internal RC 48MHz)  **************//
+  CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk);
+  CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+  CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(11));//The clock of HCLK --> 4MHz
+  // CLK->PCLKDIV = (CLK_PCLKDIV_APB0DIV_DIV4 | CLK_PCLKDIV_APB1DIV_DIV4);
+  //********* UART0_MODULE  ***************************************//
+  CLK_EnableModuleClock(UART0_MODULE);
+  CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HIRC, CLK_CLKDIV0_UART0(1));
+  //********* TMR0_MODULE  ****************************************//
+  CLK_EnableModuleClock(TMR0_MODULE);
+  CLK_SetModuleClock(TMR0_MODULE, CLK_CLKSEL1_TMR0SEL_HIRC, 0);
+  SystemCoreClockUpdate();
+  //********* GPIO PA.0 PA.1 PA.2 PA.3 ****************************//
+  SYS->GPA_MFPL = (SYS->GPA_MFPL & ~(SYS_GPA_MFPL_PA0MFP_Msk)) | (SYS_GPA_MFPL_PA0MFP_GPIO); // PA.0
+  SYS->GPA_MFPL = (SYS->GPA_MFPL & ~(SYS_GPA_MFPL_PA1MFP_Msk)) | (SYS_GPA_MFPL_PA1MFP_GPIO); // PA.1
+  SYS->GPA_MFPL = (SYS->GPA_MFPL & ~(SYS_GPA_MFPL_PA2MFP_Msk)) | (SYS_GPA_MFPL_PA2MFP_GPIO); // PA.2
+  //********* UART0 RXD=PA.15 and TXD=PA.14 ***********************//
+  SYS->GPA_MFPH = (SYS->GPA_MFPH & ~(SYS_GPA_MFPH_PA14MFP_Msk | SYS_GPA_MFPH_PA15MFP_Msk)) |
+                  (SYS_GPA_MFPH_PA15MFP_UART0_RXD | SYS_GPA_MFPH_PA14MFP_UART0_TXD);
+
+  SYS_LockReg();
+}
+//********************************************************************
+void lcd_begin(void) {
+  GPIO_SetMode(PA, BIT0, GPIO_MODE_OUTPUT); // PA.0 --> CS
+  GPIO_SetMode(PA, BIT1, GPIO_MODE_OUTPUT); // PA.1 --> WR
+  GPIO_SetMode(PA, BIT2, GPIO_MODE_OUTPUT); // PA.2 --> Data
+  config();
+}
+//********************************************************************
+int main(void) {
+  SYS_Init();
+  TIMER_Delay(TIMER0, 10);
+  lcd_begin();
+  clear();
+  while (1) {
+    setBatteryLevel(1);
+    TIMER_Delay(TIMER0, 1000000);
+    setBatteryLevel(2);
+    TIMER_Delay(TIMER0, 1000000);
+    setBatteryLevel(3);
+    TIMER_Delay(TIMER0, 1000000);
+    setBatteryLevel(0);
+    TIMER_Delay(TIMER0, 1000000);
+  }
+}
+//********************************************************************
+void wrDATA(uint8_t data, uint8_t len) {
+  for (uint8_t i = 0; i < len; i++) {
+    PA->DOUT &= ~(1 << WR);
+    PA->DOUT = (data & 0x80) ? PA->DOUT | (1 << DATA) : PA->DOUT & ~(1 << DATA);
+    // PA->DOUT &= ~(1 << WR);
+    PA->DOUT |= (1 << WR);
+    data = (data << 1);
+  }
+}
+
+void wrCMD(uint8_t CMD) {
+  PA->DOUT &= ~(1 << CS);
+  wrDATA(0x80, 4);
+  wrDATA(CMD, 8);
+  PA->DOUT |= (1 << CS);
+}
+
+void config(void) {
+  wrCMD(BIAS);
+  wrCMD(RC256);
+  wrCMD(SYSDIS);
+  wrCMD(WDTDIS1);
+  wrCMD(SYSEN);
+  wrCMD(LCDON);
+}
+
+void clear(void) {
+  wrCLR(16);
+}
+
+void wrCLR(uint8_t len) {
+  uint8_t addr = 0;
+  uint8_t i;
+  for (i = 0; i < len; i++) {
+    wrclrdata(addr, 0x00);
+    addr = addr + 2;
+  }
+}
+
+void wrclrdata(uint8_t addr, uint8_t sdata) {
+  addr <<= 2;
+  PA->DOUT &= ~(1 << CS);
+  wrDATA(0xa0, 3);
+  wrDATA(addr, 6);
+  wrDATA(sdata, 8);
+  PA->DOUT |= (1 << CS);
+}
+
+void update(void) {
+  wrone(0, _buffer[5]);
+  wrone(2, _buffer[4]);
+  wrone(4, _buffer[3]);
+  wrone(6, _buffer[2]);
+  wrone(8, _buffer[1]);
+  wrone(10, _buffer[0]);
+}
+
+void wrone(uint8_t addr, uint8_t sdata) {
+  addr <<= 2;
+  PA->DOUT &= ~(1 << CS);
+  wrDATA(0xa0, 3);
+  wrDATA(addr, 6);
+  wrDATA(sdata, 8);
+  PA->DOUT |= (1 << CS);
+}
+
+void setBatteryLevel(int level) {
+
+  _buffer[0] &= 0x7F;
+  _buffer[1] &= 0x7F;
+  _buffer[2] &= 0x7F;
+
+  switch (level) {
+  case 3: //индикатор заряда батареи включен горят все 3 сегмента
+    _buffer[0] |= 0x80;
+  case 2: //индикатор заряда батареи включен  горит 2 сегмента
+    _buffer[1] |= 0x80;
+  case 1: //индикатор заряда батареи включен  горит 1 сегмент
+    _buffer[2] |= 0x80;
+  case 0: //индикатор заряда батареи выключен
+  default:
+    break;
+  }
+  update();
+}
