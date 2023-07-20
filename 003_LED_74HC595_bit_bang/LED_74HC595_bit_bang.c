3057acd1732f33eb4feeec7b051292d0523139e0
Николай Пивнев <skuratowaanna@yandex.ru>
2023-07-20 17:17:41 +0300

    74HC595_bit_bang

--- 003_LED_74HC595_bit_bang/LED_74HC595_bit_bang.c
+++ 003_LED_74HC595_bit_bang/LED_74HC595_bit_bang.c
@@ -1,5 +1,5 @@
 /**---------------------------------------------
- *\date  15.07.2021
+ *\date  20.07.2021
  *  http://publicatorbar.ru/2017/12/21/arduino-multi-function-shield/
  * В примере используется ускоренный bit bang режим (любые пины).
  *
@@ -22,15 +22,15 @@
 #include "stdbool.h"
 #include <stdio.h>
 
-#define CLK_Pin     1
-#define LATCH_Pin   0
-#define DATA_Pin    2 
+#define CLK_Pin 1   // PA.1 --> CLK_Pin      WR
+#define LATCH_Pin 0 // PA.0 --> LATCH_Pin    CS
+#define DATA_Pin 2  // PA.2 --> DATA_Pin     DATA
 
-const uint8_t SEGMENT_SELECT[] = { 0xF7, 0xFB, 0xFD, 0xFE };
+const uint8_t SEGMENT_SELECT[] = {0xF7, 0xFB, 0xFD, 0xFE};
 uint8_t symbol[4];
-//int i = 0;
-//float z = 0.0;
-//bool variable = false;
+int i = 0;
+float z = 0.0;
+volatile bool variable = false;
 
 void send(uint8_t data);
 void WriteNumberToSegment(uint8_t Segment, uint8_t Value);
@@ -39,14 +39,69 @@ void Display_the_value(uint8_t rg, uint32_t value);
 void Display_the_Float_value(uint8_t rg, uint32_t value);
 void Display_Send_int32_t(int32_t value);
 void Display_Send_float(float value);
+/********************************************************************/
+void TMR0_IRQHandler(void) {
+  if (TIMER_GetIntFlag(TIMER0) == 1) {
+    TIMER_ClearIntFlag(TIMER0);
+    i++;
+    if (i == 999) {
+      TIMER_DisableInt(TIMER0);
+      TIMER_EnableInt(TIMER1);
+      NVIC_EnableIRQ(TMR1_IRQn);
+      NVIC_DisableIRQ(TMR0_IRQn);
+    }
+  }
+}
 
+void TMR1_IRQHandler(void) {
+  if (TIMER_GetIntFlag(TIMER1) == 1) {
+    TIMER_ClearIntFlag(TIMER1);
+    i--;
+    if (i == -999) {
+      TIMER_DisableInt(TIMER1);
+      TIMER_EnableInt(TIMER2);
+      NVIC_EnableIRQ(TMR2_IRQn);
+      NVIC_DisableIRQ(TMR1_IRQn);
+      variable = true;
+      i = 0;
+    }
+  }
+}
+
+void TMR2_IRQHandler(void) {
+  if (TIMER_GetIntFlag(TIMER2) == 1) {
+    TIMER_ClearIntFlag(TIMER2);
+    z += 0.1;
+    if (z > 99.9f) {
+      TIMER_DisableInt(TIMER2);
+      TIMER_EnableInt(TIMER3);
+      NVIC_EnableIRQ(TMR3_IRQn);
+      NVIC_DisableIRQ(TMR2_IRQn);
+    }
+  }
+}
+
+void TMR3_IRQHandler(void) {
+  if (TIMER_GetIntFlag(TIMER3) == 1) {
+    TIMER_ClearIntFlag(TIMER3);
+    z -= 0.1;
+    if (z < -99.9f) {
+      TIMER_DisableInt(TIMER3);
+      TIMER_EnableInt(TIMER0);
+      NVIC_EnableIRQ(TMR0_IRQn);
+      NVIC_DisableIRQ(TMR3_IRQn);
+      variable = false;
+      z = 0.0;
+    }
+  }
+}
+/********************************************************************/
 void SYS_Init(void) {
   SYS_UnlockReg();
   //********* Enable HIRC clock (Internal RC 48MHz)  **************//
   CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk);
   CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
   CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(11)); // The clock of HCLK --> 4MHz
-  // CLK->PCLKDIV = (CLK_PCLKDIV_APB0DIV_DIV4 | CLK_PCLKDIV_APB1DIV_DIV4);
   //********* UART0_MODULE  ***************************************//
   CLK_EnableModuleClock(UART0_MODULE);
   CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HIRC, CLK_CLKDIV0_UART0(1));
@@ -70,7 +125,7 @@ void SYS_Init(void) {
 
   SYS_LockReg();
 }
-
+/********************************************************************/
 void GPIO_Init(void) {
   GPIO_SetMode(PA, BIT0, GPIO_MODE_OUTPUT); // PA.0 --> LATCH_Pin
   GPIO_SetMode(PA, BIT1, GPIO_MODE_OUTPUT); // PA.1 --> CLK_Pin
@@ -78,19 +133,39 @@ void GPIO_Init(void) {
 }
 
 void TIMER_Init(void) {
- TIMER_Open(TIMER0, TIMER_PERIODIC_MODE, 4000000);
- TIMER_EnableInt(TIMER0);
+  TIMER_Open(TIMER0, TIMER_PERIODIC_MODE, 500);
+  //500--> это делитель (смотрим функцию uint32_t TIMER_Open(TIMER_T *timer, uint32_t u32Mode, uint32_t u32Freq))
+  TIMER_EnableInt(TIMER0);
+  TIMER_Open(TIMER1, TIMER_PERIODIC_MODE, 500);
+  TIMER_DisableInt(TIMER1);
+  TIMER_Open(TIMER2, TIMER_PERIODIC_MODE, 500);
+  TIMER_DisableInt(TIMER2);
+  TIMER_Open(TIMER3, TIMER_PERIODIC_MODE, 500);
+  TIMER_DisableInt(TIMER3);
 
+  NVIC_EnableIRQ(TMR0_IRQn);
+  NVIC_DisableIRQ(TMR1_IRQn);
+  NVIC_DisableIRQ(TMR2_IRQn);
+  NVIC_DisableIRQ(TMR3_IRQn);
+
+  TIMER_Start(TIMER0);
+  TIMER_Start(TIMER1);
+  TIMER_Start(TIMER2);
+  TIMER_Start(TIMER3);
 }
 
 int main(void) {
   SYS_Init();
   GPIO_Init();
+  TIMER_Init();
   while (1) {
-  Display_Send_float(-1000.0);
+    if (!variable)
+      Display_Send_int32_t(i);
+    else
+      Display_Send_float(z);
   }
 }
-
+/********************************************************************/
 void send(uint8_t data) {
   for (uint8_t i = 8; i >= 1; i--) {
     PA->DOUT &= ~(1 << CLK_Pin);
@@ -99,109 +174,110 @@ void send(uint8_t data) {
     data = (data << 1);
   }
 }
-
+/********************************************************************/
+/* отправляем значение (символ) в один из четырех разрядов дисплея */
 void WriteNumberToSegment(uint8_t Segment, uint8_t Value) {
   PA->DOUT &= ~(1 << LATCH_Pin);
   send(symbol[Value]);
   send(SEGMENT_SELECT[Segment]);
   PA->DOUT |= (1 << LATCH_Pin);
 }
-
+/********************************************************************/
 void SEND_SYMBOL(void) {
-  WriteNumberToSegment(0, 0);  //записываем в 0 разряд
-  WriteNumberToSegment(1, 1);  //записываем в 1 разряд
-  WriteNumberToSegment(2, 2);  //записываем в 2 разряд 
-  WriteNumberToSegment(3, 3);  //записываем в 3 разряд 
+  WriteNumberToSegment(0, 0); // записываем в 0 разряд
+  WriteNumberToSegment(1, 1); // записываем в 1 разряд
+  WriteNumberToSegment(2, 2); // записываем в 2 разряд
+  WriteNumberToSegment(3, 3); // записываем в 3 разряд
 }
-
+/********************************************************************/
 void Display_the_value(uint8_t rg, uint32_t value) {
   switch (value) {
-    case 0:
-      symbol[rg] = 0x3F;  // 0
-      break;
-    case 1:
-      symbol[rg] = 0x6;  // 1
-      break;
-    case 2:
-      symbol[rg] = 0x5B;  // 2
-      break;
-    case 3:
-      symbol[rg] = 0x4F;  // 3
-      break;
-    case 4:
-      symbol[rg] = 0x66;  // 4
-      break;
-    case 5:
-      symbol[rg] = 0x6D;  // 5
-      break;
-    case 6:
-      symbol[rg] = 0x7D;  // 6
-      break;
-    case 7:
-      symbol[rg] = 0x7;  // 7
-      break;
-    case 8:
-      symbol[rg] = 0x7F;  // 8
-      break;
-    case 9:
-      symbol[rg] = 0X6F;  // 9
-      break;
-    case 254:
-      symbol[rg] = 0X40;  // - минус
-      break;
-    case 255:
-      symbol[rg] = 0x00;  // - пустота
-      break;
-    case 256:
-      symbol[rg] = 0X8;  //- подчеркивание
-      break;
+  case 0:
+    symbol[rg] = 0x3F; // 0
+    break;
+  case 1:
+    symbol[rg] = 0x6; // 1
+    break;
+  case 2:
+    symbol[rg] = 0x5B; // 2
+    break;
+  case 3:
+    symbol[rg] = 0x4F; // 3
+    break;
+  case 4:
+    symbol[rg] = 0x66; // 4
+    break;
+  case 5:
+    symbol[rg] = 0x6D; // 5
+    break;
+  case 6:
+    symbol[rg] = 0x7D; // 6
+    break;
+  case 7:
+    symbol[rg] = 0x7; // 7
+    break;
+  case 8:
+    symbol[rg] = 0x7F; // 8
+    break;
+  case 9:
+    symbol[rg] = 0X6F; // 9
+    break;
+  case 254:
+    symbol[rg] = 0X40; // - минус
+    break;
+  case 255:
+    symbol[rg] = 0x00; // - пустота
+    break;
+  case 256:
+    symbol[rg] = 0X8; //- подчеркивание
+    break;
   }
 }
-
+/********************************************************************/
 void Display_the_Float_value(uint8_t rg, uint32_t value) {
   switch (value) {
-    case 0:
-      symbol[rg] = 0xBF;  // 0.
-      break;
-    case 1:
-      symbol[rg] = 0x86;  // 1.
-      break;
-    case 2:
-      symbol[rg] = 0xDB;  // 2.
-      break;
-    case 3:
-      symbol[rg] = 0xCF;  // 3.
-      break;
-    case 4:
-      symbol[rg] = 0xE6;  // 4.
-      break;
-    case 5:
-      symbol[rg] = 0xED;  // 5.
-      break;
-    case 6:
-      symbol[rg] = 0xFD;  // 6.
-      break;
-    case 7:
-      symbol[rg] = 0x87;  // 7.
-      break;
-    case 8:
-      symbol[rg] = 0xFF;  // 8.
-      break;
-    case 9:
-      symbol[rg] = 0XEF;  // 9.
-      break;
-    case 254:
-      symbol[rg] = 0X40;  // - минус
-      break;
-    case 255:
-      symbol[rg] = 0x00;  // - пустота
-      break;
-    case 256:
-      symbol[rg] = 0X8;  // - подчеркивание
-      break;
+  case 0:
+    symbol[rg] = 0xBF; // 0.
+    break;
+  case 1:
+    symbol[rg] = 0x86; // 1.
+    break;
+  case 2:
+    symbol[rg] = 0xDB; // 2.
+    break;
+  case 3:
+    symbol[rg] = 0xCF; // 3.
+    break;
+  case 4:
+    symbol[rg] = 0xE6; // 4.
+    break;
+  case 5:
+    symbol[rg] = 0xED; // 5.
+    break;
+  case 6:
+    symbol[rg] = 0xFD; // 6.
+    break;
+  case 7:
+    symbol[rg] = 0x87; // 7.
+    break;
+  case 8:
+    symbol[rg] = 0xFF; // 8.
+    break;
+  case 9:
+    symbol[rg] = 0XEF; // 9.
+    break;
+  case 254:
+    symbol[rg] = 0X40; // - минус
+    break;
+  case 255:
+    symbol[rg] = 0x00; // - пустота
+    break;
+  case 256:
+    symbol[rg] = 0X8; // - подчеркивание
+    break;
   }
 }
-
+/********************************************************************/
 void Display_Send_int32_t(int32_t value) {
   if (value > 9999 || value < -999) {
     Display_the_value(0, 256);
@@ -218,7 +294,7 @@ void Display_Send_int32_t(int32_t value) {
       positive_value = true;
     }
     uint8_t BCD_Arr[4] = {
-      0,
+        0,
     };
     uint32_t lenght;
     lenght = value;
@@ -283,7 +359,7 @@ void Display_Send_int32_t(int32_t value) {
     }
   }
 }
-
+/********************************************************************/
 void Display_Send_float(float value) {
   if (value > 999.9f || value < -99.9f) {
     Display_the_Float_value(0, 256);
@@ -292,10 +368,10 @@ void Display_Send_float(float value) {
     Display_the_Float_value(3, 256);
     SEND_SYMBOL();
   } else {
-    //умножаем на 10 т.к количество знаков после запятой один!
+    // умножаем на 10 т.к количество знаков после запятой один!
     int32_t value_conv = value * 10;
     value = value * 10;
-    //знак -------------------
+    // знак -------------------
     bool positive_value;
     if (value_conv < 0) {
       positive_value = false;
@@ -303,9 +379,9 @@ void Display_Send_float(float value) {
     } else {
       positive_value = true;
     }
-    //массив ------------------
+    // массив ------------------
     uint8_t BCD_Arr[4] = {
-      0,
+        0,
     };
     int32_t lenght;
     lenght = value_conv;
