bcb22d453cd9ba5da8597bff9c296ee2484a1ca7
Николай Пивнев <skuratowaanna@yandex.ru>
2023-08-13 23:01:49 +0300

    button led

--- 001_GPIO/002_Button_Led.c
+++ 001_GPIO/002_Button_Led.c
@@ -37,6 +37,18 @@
 #include "stdbool.h"
 #include <stdio.h>
 
+typedef uint32_t  u32;
+typedef uint16_t  u16;
+typedef uint8_t   u8;
+
+u8 condition = TRUE;
+
+//#define TRUE      (1UL) --> Boolean true, define to use in API parameters or return value
+//#define FALSE     (0UL) --> Boolean false, define to use in API parameters or return value
+
+//#define ENABLE    (1UL) --> Enable, define to use in API parameters
+//#define DISABLE   (0UL) --> Disable, define to use in API parameters
+
 void SYS_Init(void) {
   SYS_UnlockReg();
   //********* Enable HIRC clock (Internal RC 48MHz)  **************//
@@ -52,12 +64,15 @@ void SYS_Init(void) {
   CLK_SetModuleClock(TMR0_MODULE, CLK_CLKSEL1_TMR0SEL_HIRC, 0);
 
   SystemCoreClockUpdate();
-  //********* GPIO PA.0 PA.1 PA.2 PA.3 ****************************//
+  //********* GPIO PA.0 PA.1 PA.2 PA.3 ****************************//output
   SYS->GPA_MFPL = (SYS->GPA_MFPL & ~(SYS_GPA_MFPL_PA0MFP_Msk)) | (SYS_GPA_MFPL_PA0MFP_GPIO); // PA.0
   SYS->GPA_MFPL = (SYS->GPA_MFPL & ~(SYS_GPA_MFPL_PA1MFP_Msk)) | (SYS_GPA_MFPL_PA1MFP_GPIO); // PA.1
   SYS->GPA_MFPL = (SYS->GPA_MFPL & ~(SYS_GPA_MFPL_PA2MFP_Msk)) | (SYS_GPA_MFPL_PA2MFP_GPIO); // PA.2
-  SYS->GPA_MFPL = (SYS->GPA_MFPL & ~(SYS_GPA_MFPL_PA3MFP_Msk)) | (SYS_GPA_MFPL_PA3MFP_GPIO); // PA.3
 
+  //********* GPIO PB.0 PB.1 PB.2 ****************************//input
+  SYS->GPB_MFPL = (SYS->GPB_MFPL & ~(SYS_GPB_MFPL_PB0MFP_Msk)) | (SYS_GPB_MFPL_PB0MFP_GPIO); // PB.0
+  SYS->GPB_MFPL = (SYS->GPB_MFPL & ~(SYS_GPB_MFPL_PB1MFP_Msk)) | (SYS_GPB_MFPL_PB1MFP_GPIO); // PB.1
+  SYS->GPB_MFPL = (SYS->GPB_MFPL & ~(SYS_GPB_MFPL_PB2MFP_Msk)) | (SYS_GPB_MFPL_PB2MFP_GPIO); // PB.2
   //********* UART0 RXD=PA.15 and TXD=PA.14 ***********************//
   SYS->GPA_MFPH = (SYS->GPA_MFPH & ~(SYS_GPA_MFPH_PA14MFP_Msk | SYS_GPA_MFPH_PA15MFP_Msk)) |
                   (SYS_GPA_MFPH_PA15MFP_UART0_RXD | SYS_GPA_MFPH_PA14MFP_UART0_TXD);
@@ -69,11 +84,14 @@ void LED_GPIO_init(void) {
   GPIO_SetMode(PA, BIT0, GPIO_MODE_OUTPUT);
   GPIO_SetMode(PA, BIT1, GPIO_MODE_OUTPUT);
   GPIO_SetMode(PA, BIT2, GPIO_MODE_OUTPUT);
-  GPIO_SetMode(PA, BIT3, GPIO_MODE_OUTPUT);
   PA0 = 1;
   PA1 = 1;
   PA2 = 1;
-  PA3 = 1;
+  GPIO_SetMode(PB, BIT0, GPIO_MODE_INPUT);
+  GPIO_SetMode(PB, BIT1, GPIO_MODE_INPUT);
+  GPIO_SetMode(PB, BIT2, GPIO_MODE_INPUT);
+  GPIO_SET_DEBOUNCE_TIME(GPIO_DBCTL_DBCLKSRC_LIRC,  GPIO_DBCTL_DBCLKSEL_512);
+  GPIO_ENABLE_DEBOUNCE(PB, BIT0 | BIT1 | BIT2);
 }
 
 int main(void) {
@@ -81,8 +99,12 @@ int main(void) {
   LED_GPIO_init();
 
   while (1) {
-    
+  condition = PB0;
+  (condition == FALSE)?(PA0 = DISABLE):(PA0 = ENABLE);  
+  condition = PB1;
+  (condition == FALSE)?(PA1 = DISABLE):(PA1 = ENABLE); 
+  condition = PB2;
+  (condition == FALSE)?(PA2 = DISABLE):(PA2 = ENABLE);
   }
 }
-
 /****************************** End of file *****************************/
